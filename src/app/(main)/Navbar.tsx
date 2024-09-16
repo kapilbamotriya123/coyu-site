91a63aa8cede4b1df930cd7b42197cafad74d0e5
kapilbamotriya123 <kapilbamotriya12345@gmail.com>
2024-09-16 10:43:51 +0530

    navbar changes

--- src/app/(main)/Navbar.tsx
+++ src/app/(main)/Navbar.tsx
@@ -1,26 +1,50 @@
+'use client'
+
+import { useState, useEffect } from 'react';
 import Image from 'next/image';
 import logo from '../ui/assets/coyu.svg';
 import { ShoppingBag } from 'lucide-react';
-
 import hamburger from '../ui/assets/hamburger.svg';
-import shopping_icon from '../ui/assets/shopping-bag-icon.svg';
 
 const Navbar = () => {
-   return (
-      <div className="sticky top-0 z-50 flex w-full items-center justify-between px-4 pt-5">
-         <div className="">
-            <Image src={hamburger} width={24} alt="hamburger" />
-         </div>
+  const [showNavbar, setShowNavbar] = useState(true);
+  let lastScrollY = 0;
 
-         <div className="">
-            <Image src={logo} width={150} className="App-logo" alt="logo" />
-         </div>
+  const handleScroll = () => {
+    if (window.scrollY > lastScrollY) {
+      setShowNavbar(false);
+    } else {
+      setShowNavbar(true);
+    }
+    lastScrollY = window.scrollY;
+  };
 
-         <div className="">
-            <ShoppingBag size={24} className="text-black" />
-         </div>
+  useEffect(() => {
+    window.addEventListener('scroll', handleScroll);
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, []);
+
+  return (
+    <div
+      className={`sticky top-0 z-[10000] flex w-full items-center justify-between px-4 py-3 transition-transform duration-300 bg-slate-200 bg-opacity-50 backdrop-blur-md ${
+        !showNavbar && '-translate-y-full'
+      }`}
+    >
+      <div>
+        <Image src={hamburger} width={24} alt="hamburger" />
       </div>
-   );
+
+      <div>
+        <Image src={logo} width={150} className="App-logo" alt="logo" />
+      </div>
+
+      <div>
+        <ShoppingBag size={24} className="text-black" />
+      </div>
+    </div>
+  );
 };
 
 export default Navbar;
